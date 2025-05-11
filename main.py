25df8e57e8bdd0d0894865cac84fdec45873b7bf
Talharana <mtalharana093@gmail.com>
2025-05-11 18:38:53 +0500

    update

--- main.py
+++ main.py
@@ -2,21 +2,91 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-# Get Method
+# # Get Method
+# @app.get("/")
+# def get_method():
+#   return {"message": "Talha Rana AI Engineer"}
+# # Put Method
+# @app.put("/")
+# def put_method():
+#   return {"message": "Talha Rana AI Engineer"}
+
+# # Post Method
+# @app.post("/")
+# def post_method():
+#   return {"message": "Talha Rana AI Engineer"}
+
+# # Delete Method
+# @app.delete("/")
+# def delete_method():
+#   return {"message": "Talha Rana AI Engineer"}
+
+
 @app.get("/")
 def get_method():
   return {"message": "Talha Rana AI Engineer"}
-# Put Method
-@app.put("/")
-def put_method():
-  return {"message": "Talha Rana AI Engineer"}
 
-# Post Method
-@app.post("/")
-def post_method():
-  return {"message": "Talha Rana AI Engineer"}
 
-# Delete Method
-@app.delete("/")
-def delete_method():
-  return {"message": "Talha Rana AI Engineer"}
+
+data = [
+{
+  "id": 1,
+  "name": "Shahjahan",
+  "age": 4,
+  "email": "talharana@gmail.com",
+  "phone": "03001234567"
+  
+},
+{
+  "id": 2,
+  "name": "Talha Rana",
+  "age": 33,
+  "email": "talharana@gmail.com",
+  "phone": "03001234567"
+  
+},
+{
+  "id": 3,
+  "name": "Ali Rana",
+  "age": 25,
+  "email": "talharana@gmail.com",
+  "phone": "03001234567"
+  
+},
+{
+  "id": 4,
+  "name": "Danyal Rana",
+  "age": 27,
+  "email": "talharana@gmail.com",
+  "phone": "03001234567"
+  
+},
+]
+
+# @app.get("/login")
+# def login():
+#   return {
+#     "data": data,
+#     "message": "Talha Rana AI Engineer",
+#     "status": "success",
+    
+#   }
+
+@app.get("/login")
+def login():
+  try:
+    return {
+      
+      "data": [],
+      "message": "Login Successful",
+      "status": "success",
+    }
+    print("Login Successful", data)
+  except Exception as e:
+    return {
+      # "data": [],
+      "message": "Login Failed",
+      "status": 500,
+      "error": str(e)
+    }
+    

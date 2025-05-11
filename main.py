480d1ebf69803de6f3ad5f35b502d397d1f6dd58
Talharana <mtalharana093@gmail.com>
2025-05-11 17:43:29 +0500

    api methods

--- main.py
+++ main.py
@@ -2,6 +2,21 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# Get Method
 @app.get("/")
 def get_method():
+  return {"message": "Talha Rana AI Engineer"}
+# Put Method
+@app.put("/")
+def put_method():
+  return {"message": "Talha Rana AI Engineer"}
+
+# Post Method
+@app.post("/")
+def post_method():
+  return {"message": "Talha Rana AI Engineer"}
+
+# Delete Method
+@app.delete("/")
+def delete_method():
   return {"message": "Talha Rana AI Engineer"}
